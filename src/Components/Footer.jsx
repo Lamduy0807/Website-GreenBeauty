7faf966a8cce21d41d2c9e94e85387863856dcd3
Lamduy0807 <19521421@gm.uit.edu.vn>
2022-03-30 19:45:19 +0700

    add Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <div className="row">
+          
+          <div className="col-1-of-4">
+            <h2 className="footer__h2">Chăm sóc khách hàng</h2>
+            <ul className="footer__ul">
+              <Link className="footer__li">Trung tâm giúp đỡ</Link>
+              <Link className="footer__li">Hướng dẫn mua hàng</Link>
+              <Link className="footer__li">Chăm sóc khách hàng</Link>
+              <Link className="footer__li">Trả hàng hoàn tiền</Link>
+            </ul>
+          </div>
+          <div className="col-1-of-4">
+            <h2 className="footer__h2">Về GreenBeauty</h2>
+            <Link className="footer__li">Giới thiệu</Link>
+              <Link className="footer__li">Tuyển dụng</Link>
+              <Link className="footer__li">Liên hệ</Link>
+          </div>
+          <div className="col-1-of-4">
+            <h2 className="footer__h2">Theo dõi chúng tôi</h2>
+            <Link className="footer__li text-align">
+              <i class='bx bxl-facebook-square margin-right text-align'></i>
+              Facebook</Link>
+              <Link className="footer__li text-align">
+              <i class='bx bxl-instagram margin-right text-align' ></i>
+              Instagram</Link>
+          </div>
+          <div className="col-1-of-4">
+            <p>
+              <Link to="/">
+                <img src={require("../assets/img/logo1.png")} className="footer__logo" alt="" />
+              </Link>
+            </p>
+            <p className="footer__p"> 
+              Hãy cùng GreenBeauty hướng đến một cuộc sống
+              năng động, tích cực với làn da tươi trẻ và khỏe mạnh.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
